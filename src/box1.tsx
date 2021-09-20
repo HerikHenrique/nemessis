435c99c0396e6b07f7af6a5e6af7491dbf30bbe9
HerikHenrique <herikcontato@gmail.com>
2021-09-20 00:56:51 -0300

    add Box1 Component

---
+++ src/box1.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+class Box1 extends Component {
+    render(){
+        return(
+            <div>
+                <p>Testando</p>
+            </div>
+        )
+    }
+}
+export default Box1;
